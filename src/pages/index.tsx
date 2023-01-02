14aa03a269daf43c7f09261c0a1ea3f362eecd8f
gull2365 <jych2365@gmail.com>
2023-01-02 23:23:12 +0900

    feat: 테스트 코드 작성

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import Text from 'components/text'
+
+const IndexPage: FunctionComponent = function () {
+  return <Text text="Home" />
+}
+
+export default IndexPage
