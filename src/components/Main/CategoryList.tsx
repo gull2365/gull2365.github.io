9377925c1f318928ee39794859f4beb47e8be7f4
gull2365 <jych2365@gmail.com>
2023-03-18 18:34:47 +0900

    feat: 메인 페이지 반응형 추가

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -24,19 +24,30 @@ const CategoryListWrapper = styled.div`
   flex-wrap: wrap;
   width: 768px;
   margin: 100px auto 0;
+
+  @media (max-width: 768px) {
+    width: 100px;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
 `
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
   <Link {...props} />
 ))<CategoryItemProps>`
   margin-right: 20px;
   padding: 5px 0;
   font-size: 18px;
-  font-weight: ${({ active }) => (active ? `800` : `400`)};
+  font-weight: ${({ active }) => (active ? '800' : '400')};
   cursor: pointer;
 
-  &:last-of-tyoe {
+  &:last-of-type {
     margin-right: 0;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 15px;
   }
 `
 
