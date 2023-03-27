cd799136ff5b7f34ea522c0feb37d435f3315e1b
Gull 2365 <jych2365@gmail.com>
2023-03-27 23:13:02 +0900

    feat: 마크다운 데이터에 Slug 필드 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: <https://www.gatsbyjs.com/docs/node-apis/>
- */
-
-// You can delete this file if you're not using it
-
 const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
@@ -22,4 +15,14 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
       },
     },
   })
+}
+
+exports.onCreateNode = ({ node, getNode, action }) => {
+  const { onCreateNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    onCreateNodeField({ node, name: 'slug', value: slug })
+  }
 }
