fee3c8d307c8c751f28c58104965f0dc4e338dbd
Gull 2365 <jych2365@gmail.com>
2023-03-31 23:03:08 +0900

    feat: posts 배열에서 postList 배열로 변경

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useMemo } from 'react'
+import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import { PostListItemType } from 'types/PostItem.types'
 import PostItem from './PostItem'
@@ -52,7 +52,7 @@ const PostList: FunctionComponent<PostListProps> = function ({
 
   return (
     <PostListWrapper ref={containerRef}>
-      {posts.map(
+      {postList.map(
         ({
           node: {
             id,
