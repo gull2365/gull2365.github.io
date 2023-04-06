f1e218cfed34f540edef450bc73aa9158d141e90
Gull 2365 <jych2365@gmail.com>
2023-04-06 18:28:00 +0900

    feat: 소개 수정

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -57,7 +57,7 @@ const Introduction: FunctionComponent<IntroductionProps> = function ({
 
         <div>
           <SubTitle>Nice to Meet You,</SubTitle>
-          <Title>I'm Junior Frontend Developer Hyun.</Title>
+          <Title>I'm Junior Frontend Developer Yochan.</Title>
         </div>
       </Wrapper>
     </Background>
